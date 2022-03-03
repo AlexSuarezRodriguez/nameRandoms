e682b8d8209642fe4a14908b482a1c30f6edd31b
AlexSuarezRodriguez <alex.suarez@hotmail.com>
2022-03-02 21:30:13 -0500

    feature: correction feedback,print console message if-else

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const arr=[];
 for(i=1;i<=1000;i++){
   const randomName = faker.name.findName();
   arr.push({numero:i,
-    name:randomName})
+    name:randomName});
 }
-//fileSystem.writeFile('names.txt', NodeJS.ArrayBufferView(arr))
-fileSystem.writeFileSync('names.txt',JSON.stringify(arr))
-console.log("file of names created")
+fileSystem.writeFileSync('names.txt',JSON.stringify(arr));
+arr.length===1000 ? console.log("created file") : console.log("error")
